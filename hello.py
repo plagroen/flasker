5991209cb2c9c686350f591a1931d3d8bc3dc0a9
Groeneveld <philip.groeneveld@centric.eu>
2022-01-21 18:05:05 +0100

    Up untill and including Flask Friday #8

--- hello.py
+++ hello.py
@@ -1,11 +1,52 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Create a Flask Instance
 app = Flask(__name__)
+# add database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# secret key (CSRF token)
+app.config['SECRET_KEY'] = "my super secret key that nobody is supposed to know"
+
+# Initialize the database
+db = SQLAlchemy(app)
+# Create Model
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(200), nullable=False)
+	email = db.Column(db.String(120), nullable=False, unique=True)
+	date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+	# Create a String
+	def __repr__(self):
+		return '<Name %r>' % self.name
+
+# Create a Form Class
+class UserForm(FlaskForm):
+	name = StringField("Name", validators=[DataRequired()])
+	email = StringField("Email", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+	name = StringField("What's your name", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
 
 # Create a route decorator
 @app.route('/')
+def index():
+	first_name = "John"
+	stuff = "This is bold text."
+	favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
+	return render_template("index.html",
+		first_name=first_name, 
+		stuff=stuff,
+		favorite_pizza=favorite_pizza)
 
 #def index():
 #	return "<h1>Hello World!</h1>"
@@ -19,15 +60,25 @@ app = Flask(__name__)
 # trim
 # striptags
 
-
-def index():
-	first_name = "John"
-	stuff = "This is bold text."
-	favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
-	return render_template("index.html",
-		first_name=first_name, 
-		stuff=stuff,
-		favorite_pizza=favorite_pizza)
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+	name = None
+	form = UserForm()
+	if form.validate_on_submit():
+		user = Users.query.filter_by(email=form.email.data).first()
+		if user is None:
+			user = Users(name=form.name.data, email=form.email.data)
+			db.session.add(user)
+			db.session.commit()
+		name = form.name.data
+		form.name.data = ''
+		form.email.data = ''
+		flash("User " + name + " added sucessfully!")
+	our_users = Users.query.order_by(Users.date_added)
+	return render_template("add_user.html",
+		form=form,
+		name=name,
+		our_users=our_users)
 
 # localhost:5000/user/john
 @app.route('/user/<name>')
@@ -46,3 +97,18 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+	name = None
+	form = NamerForm()
+	# Validate form
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = '' 
+		flash("Form submitted succesfully!")
+
+	return render_template("name.html",
+		name = name,
+		form = form)	
